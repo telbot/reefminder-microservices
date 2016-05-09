db1323a81a5ad1ee9ccaf638b52b4f982d200c65
David Bingham <david.r.bingham@gmail.com>
2016-05-09 09:07:35 +0100

    Adding Base Services

---
+++ build-all-projects.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd api-gateway; ./gradlew clean build; cd ..
+cd auth-server; ./gradlew clean build; cd ..
+cd config-server; ./gradlew clean build; cd ..
+cd task-webservice; ./gradlew clean build; cd ..
+cd user-webservice; ./gradlew clean build; cd ..
+cd webservice-registry; ./gradlew clean build; cd ..
+cd comments-webservice; ./gradlew clean build; cd ..
